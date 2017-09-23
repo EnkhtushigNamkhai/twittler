c38cedfacb1924f2dcef0e0cfe8c4303d5dd7c1f
EnkhtushigNamkhai <enkhtushig@gmail.com>
2017-09-22 22:11:43 -0700

    Added Writing Post functionality, need to fix date order

--- application.js
+++ application.js
@@ -38,6 +38,57 @@ $(document).ready(function(){
     $('.Tweets').html('');
   });
 
+  $('.newPostLink').on('click', function() {
+    $('.Post').slideDown();
+    $(this).hide();
+  });
+
+  $('#PostCancel').on('click', function() {
+    $('.Post').hide();
+    $('.newPostLink').show();
+  });
+
+  $('#NewPostSubmit').on('click', function(){
+    //read the username and post
+    var userName = $('#userNameInput').val();
+    var content = $('#ContentInput').val();
+    if (streams.users[userName] === undefined) {
+      streams.users[userName] = [];
+    }
+     
+    var date = new Date();
+    var tweet = {};
+    tweet.user = userName;
+    tweet.message = content;
+    tweet.created_at = date;
+    addTweet(tweet);
+
+
+    var month = date.getMonth();
+    var day = date.getDate();
+    var hour =  date.getHours();
+    var min = date.getMinutes();
+    var finalDate = month + '/'+ day + ', ' + hour +':' + min;
+
+    var $tweet = $('<li></li>').addClass('tweet');
+
+    $tweet.append($('<h2></h2>').addClass('user'),
+    $('<p></p>').addClass('date'), $('<p></p>').addClass('post'),
+    $('<ul></ul>').addClass('timeline'));
+
+    $tweet.find(".user").text('@' + userName);
+    $tweet.find(".post").text(content);
+    $tweet.find(".date").text('Posted On: ' + finalDate);
+    $tweet.prependTo($("#tweet-list"));
+    //create a date element
+    //create tweet object
+    //add to streams.users[username] the tweet
+    
+    //create a new node
+    //add to html prepend? not append
+  });
+
+
 
 
 });
@@ -49,6 +100,11 @@ function loadTweets() {
   while(index >= 0){
     var tweet = streams.home[index];
     var createdDate = tweet.created_at;
+    var month = createdDate.getMonth();
+    var day = createdDate.getDate();
+    var hour =  createdDate.getHours();
+    var min = createdDate.getMinutes();
+    var finalDate = month + '/'+ day + ', ' + hour +':' + min;
 
     var $tweet = $('<li></li>').addClass('tweet');
 
@@ -58,7 +114,7 @@ function loadTweets() {
 
     $tweet.find(".user").text('@' + tweet.user);
     $tweet.find(".post").text(tweet.message);
-    $tweet.find(".date").text('Posted On: ' + tweet.created_at);
+    $tweet.find(".date").text('Posted On: ' + finalDate);
     //$(".tweet-list").append($tweet);
     $tweet.appendTo($("#tweet-list"));
     index -= 1;
@@ -83,10 +139,16 @@ function seeTimeLine() {
 
     for (var i = 0; i < tweetsOfUser.length; i++) {
       var CurTweet = tweetsOfUser[i];
-      $('.Tweets-Container').find('h3').text(userName + "'s TimeLine");
+      var createdDate = CurTweet.created_at;
+      var month = createdDate.getMonth();
+      var day = createdDate.getDate();
+      var hour =  createdDate.getHours();
+      var min = createdDate.getMinutes();
+      var finalDate = month + '/'+ day + ', ' + hour +':' + min;
+      $('.Tweets-Container').find('h3').text(userName + "'s Activities");
       var $a = $('<li></li>').addClass('usertweet');
       $a.append($('<p></p>').addClass('date'), $('<p></p>').addClass('post'));
-      $a.find('.date').text('Posted on: ' + CurTweet.created_at);
+      $a.find('.date').text('Posted on: ' + finalDate);
       $a.find('.post').text(CurTweet.message);
 
       $('.Tweets').append($a);
