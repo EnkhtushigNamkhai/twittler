8c0f9b05b7fc72996962c5bf39e84baf0d4a9d5d
EnkhtushigNamkhai <enkhtushig@gmail.com>
2017-09-24 17:21:33 -0700

    Completed Twittler

--- application.js
+++ application.js
@@ -1,8 +1,16 @@
+var fonts = [{'font-family': 'Leckerli One, cursive'},
+{'font-family': 'Chewy, cursive'}, {'font-family': 'Cabin Sketch, cursive'}, {'font-family': 'Pacifico, cursive'},
+{'font-family': 'Montserrat, sans-serif'}];
+var fontIndex = 0;
+
+
+
 $(document).ready(function(){
   var $body = $('body');
   //$body.html('');
         //how can I determine when new tweets are generated? and then display them
         //also what is the timeline suppose to have?
+  //have an global index of where I last read from
   loadTweets();
   
   $('#More').on('click',function() {
@@ -46,9 +54,97 @@ $(document).ready(function(){
   $('#PostCancel').on('click', function() {
     $('.Post').hide();
     $('.newPostLink').show();
+    $('#userNameInput').val("");
+    $('#ContentInput').val("");
   });
 
-  $('#NewPostSubmit').on('click', function(){
+  $('#NewPostSubmit').on('click', SubmitEventHandler);
+  
+  $('body').keypress(function(e){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    // console.log(keycode == '13');
+
+    if ($('.Post').is(":visible") && keycode == '13') {
+      console.log('calls func');
+      SubmitEventHandler();
+    }
+  });
+
+  // Change the fonts when the logo is clicked
+  $('.logo').on('click', function() {
+    if (fontIndex === fonts.length) {
+      fontIndex = 0;
+    }
+    console.log(fontIndex);
+    $('.logo').css(fonts[fontIndex]);
+    fontIndex++;
+  });
+});
+
+/******************* Event handler for loading the Tweets on Home Page ***********************/
+
+function loadTweets() {
+  //instead of deleting streams.home, I can make a new list that copies the streams.home and read and deletes from there
+  //if there is something new I add it to the list
+  var index = 0;
+  while(index <= streams.home.length - 1){
+    var tweet = streams.home[index];
+    var createdDate = tweet.created_at;
+    var month = createdDate.getMonth() + 1;
+    var day = createdDate.getDate();
+    var hour =  createdDate.getHours();
+    var min = createdDate.getMinutes();
+    var sec = createdDate.getSeconds();
+    var finalDate = month + '/'+ day + ', ' + hour +':' + min + ':' +sec;
+
+    var $tweet = $('<li></li>').addClass('tweet');
+    $tweet.append($('<h2></h2>').addClass('user'),
+    $('<p></p>').addClass('date'), $('<p></p>').addClass('post'),
+    $('<ul></ul>').addClass('timeline'));
+
+    $tweet.find(".user").text('@' + tweet.user);
+    $tweet.find(".post").text(tweet.message);
+    $tweet.find(".date").text('Posted On: ' + finalDate);
+    $tweet.prependTo($("#tweet-list"));
+    index += 1;
+  }
+  emptyProcessedStream();
+}
+
+function emptyProcessedStream() {
+  streams.home = [];
+}
+
+/******************* Event handler for seeing the TimeLine ***********************/
+
+function seeTimeLine() {
+  $('.Tweets').html('');
+  // Get User's name and find all the tweets of user
+  var userName = $(this).find('.user').text().substring(1);
+  var tweetsOfUser = streams.users[userName];
+  //Go through all the tweets of the user
+  for (var i = 0; i < tweetsOfUser.length; i++) {
+    var CurTweet = tweetsOfUser[i];
+    var createdDate = CurTweet.created_at;
+    var month = createdDate.getMonth() + 1;
+    var day = createdDate.getDate();
+    var hour =  createdDate.getHours();
+    var min = createdDate.getMinutes();
+    var finalDate = month + '/'+ day + ', ' + hour +':' + min;
+    $('.Tweets-Container').find('h3').text(userName + "'s Activities");
+    var $a = $('<li></li>').addClass('usertweet');
+    $a.append($('<p></p>').addClass('date'), $('<p></p>').addClass('post'));
+    $a.find('.date').text('Posted on: ' + finalDate);
+    $a.find('.post').text(CurTweet.message);
+    $('.Tweets').prepend($a);
+  }
+  //After constructing the timeline, show it
+  $('.PopUpBack').show();
+}
+
+/******************* Event handler for the Submit Button (for Posting) ***********************/
+
+function SubmitEventHandler(){
     //read the username and post
     var userName = $('#userNameInput').val();
     var content = $('#ContentInput').val();
@@ -64,7 +160,7 @@ $(document).ready(function(){
     addTweet(tweet);
 
 
-    var month = date.getMonth();
+    var month = date.getMonth() + 1;
     var day = date.getDate();
     var hour =  date.getHours();
     var min = date.getMinutes();
@@ -80,80 +176,8 @@ $(document).ready(function(){
     $tweet.find(".post").text(content);
     $tweet.find(".date").text('Posted On: ' + finalDate);
     $tweet.prependTo($("#tweet-list"));
-    //create a date element
-    //create tweet object
-    //add to streams.users[username] the tweet
-    
-    //create a new node
-    //add to html prepend? not append
-  });
-
-
-
-
-});
-
-function loadTweets() {
-  //instead of deleting streams.home, I can make a new list that copies the streams.home and read and deletes from there
-  //if there is something new I add it to the list
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var createdDate = tweet.created_at;
-    var month = createdDate.getMonth();
-    var day = createdDate.getDate();
-    var hour =  createdDate.getHours();
-    var min = createdDate.getMinutes();
-    var finalDate = month + '/'+ day + ', ' + hour +':' + min;
-
-    var $tweet = $('<li></li>').addClass('tweet');
-
-    $tweet.append($('<h2></h2>').addClass('user'),
-     $('<p></p>').addClass('date'), $('<p></p>').addClass('post'),
-      $('<ul></ul>').addClass('timeline'));
-
-    $tweet.find(".user").text('@' + tweet.user);
-    $tweet.find(".post").text(tweet.message);
-    $tweet.find(".date").text('Posted On: ' + finalDate);
-    //$(".tweet-list").append($tweet);
-    $tweet.appendTo($("#tweet-list"));
-    index -= 1;
-  }
-  emptyProcessedStream();
-}
-
-function emptyProcessedStream() {
-  streams.home = [];
-}
-
-function seeTimeLine() {
-   //get the user that was clicked
-    //go through streams.users[username].push(newTweet);
-    //list of the tweets of the user = streams.users[username]
-    //display the list of tweets
-
-    $('.Tweets').html('');
-    var userName = $(this).find('.user').text().substring(1);
-    // console.log(userName);
-    var tweetsOfUser = streams.users[userName];
-
-    for (var i = 0; i < tweetsOfUser.length; i++) {
-      var CurTweet = tweetsOfUser[i];
-      var createdDate = CurTweet.created_at;
-      var month = createdDate.getMonth();
-      var day = createdDate.getDate();
-      var hour =  createdDate.getHours();
-      var min = createdDate.getMinutes();
-      var finalDate = month + '/'+ day + ', ' + hour +':' + min;
-      $('.Tweets-Container').find('h3').text(userName + "'s Activities");
-      var $a = $('<li></li>').addClass('usertweet');
-      $a.append($('<p></p>').addClass('date'), $('<p></p>').addClass('post'));
-      $a.find('.date').text('Posted on: ' + finalDate);
-      $a.find('.post').text(CurTweet.message);
-
-      $('.Tweets').append($a);
-    }
-    $('.PopUpBack').show();
-    
-    // loadUserTweet(tweetsOfUser);  
+    $('.Post').hide();
+    $('.newPostLink').show();
+    $('#userNameInput').val("");
+    $('#ContentInput').val("");
 }
